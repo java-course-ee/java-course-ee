6829c48ddb89b1ca95ecb16a5034f219b57e98bd
Artem Pronchakov <artem.pronchakov@calisto.email>
2014-09-26 16:19:11 +0400

    log4j

---
+++ Logging/log4j/src/main/java/ru/test/logging/App.java
@@ -0,0 +1,18 @@
+package ru.test.logging;
+
+import org.apache.log4j.Logger;
+
+public class App
+{
+    private static final Logger log = Logger.getLogger( App.class );
+
+    public static void main( String[] args )
+    {
+        log.trace( "trace message" );
+        log.debug( "debug message" );
+        log.info( "info message" );
+        log.warn( "warn message" );
+        log.error( "error message" );
+        log.fatal( "fatal message" );
+    }
+}
