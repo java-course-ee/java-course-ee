534d68a1470fd673cfc996defd8a867c72c5bcd8
Artem Pronchakov <artem.pronchakov@calisto.email>
2014-11-29 17:45:18 +0300

    Add Spring Integration Router sample

---
+++ Spring/Integration/spring-integration-03/src/main/java/edu/javacourse/spring/integration/BigLogger.java
@@ -0,0 +1,12 @@
+package edu.javacourse.spring.integration;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class BigLogger {
+
+    public void log(String message){
+        System.out.println("BigLogger: " + message);
+    }
+
+}
---
+++ Spring/Integration/spring-integration-03/src/main/java/edu/javacourse/spring/integration/SmallLogger.java
@@ -0,0 +1,12 @@
+package edu.javacourse.spring.integration;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class SmallLogger {
+
+    public void log(String message){
+        System.out.println("SmallLogger: " + message);
+    }
+
+}
