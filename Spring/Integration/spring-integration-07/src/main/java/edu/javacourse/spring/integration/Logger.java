ed74bbd94a4371f8cdab225f40497121c3de5467
Artem Pronchakov <artem.pronchakov@calisto.email>
2014-11-29 19:58:20 +0300

    Add Spring Integration Inbound Adapter / Poller sample

---
+++ Spring/Integration/spring-integration-07/src/main/java/edu/javacourse/spring/integration/Logger.java
@@ -0,0 +1,12 @@
+package edu.javacourse.spring.integration;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class Logger {
+
+    public void log(String message){
+        System.out.println("Logger: " + message);
+    }
+
+}
