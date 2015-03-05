78746ec1cbb64887599a97d280dcd2340f523ee2
Artem Pronchakov <artem.pronchakov@calisto.email>
2015-03-05 03:31:11 +0300

    Update Topic MDB for transaction rollback

--- EJB/jms/jms-ejb/src/main/java/edu/javacourse/jms/MessageReceiverBean3.java
+++ EJB/jms/jms-ejb/src/main/java/edu/javacourse/jms/MessageReceiverBean3.java
@@ -5,8 +5,10 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -31,13 +33,22 @@ public class MessageReceiverBean3 implements MessageListener {
         log.debug("Bean 3 PreDestroy");
     }
 
+    @Resource
+    public MessageDrivenContext context;
+
     @Override
     public void onMessage(Message message) {
         if (message instanceof TextMessage) {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1000);
                 TextMessage textMessage = (TextMessage) message;
-                log.debug("Bean 3 from topic: {}", textMessage.getText());
+                final String text = textMessage.getText();
+                if (text.contains("This is message 3") || text.contains("This is message 5") || text.contains("This is message 7")) {
+                    log.debug("*** Bean 3 Throw message with text'"  + text + "' away...");
+                    context.setRollbackOnly();
+                    return;
+                }
+                log.debug("Bean 3 from topic: {}", text);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             } catch (JMSException ex) {
