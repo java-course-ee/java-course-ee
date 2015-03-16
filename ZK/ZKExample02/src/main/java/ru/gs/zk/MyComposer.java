4340666c9f53a52690a031a0d47e0c7805f06893
Artem Pronchakov <artem.pronchakov@calisto.email>
2015-03-16 16:00:55 +0300

    Move ZK example 02 to new API version

--- ZK/ZKExample02/src/main/java/ru/gs/zk/MyComposer.java
+++ ZK/ZKExample02/src/main/java/ru/gs/zk/MyComposer.java
@@ -1,25 +1,32 @@
 package ru.gs.zk;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Label;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
  * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
  */
-public class MyComposer extends GenericForwardComposer {
-    public static final String DATE_FORMAT = "dd.MM.yyyy HH:mm:ss";
+public class MyComposer extends SelectorComposer<Component> {
+    public final String DATE_FORMAT = "dd.MM.yyyy HH:mm:ss";
+    @Wire
     private Datebox firstDate;
+    @Wire
     private Datebox secondDate;
+    @Wire
     private Label count;
+    @Wire
     private Label someElseParam;
+    @Wire
     private Label fromDate;
+    @Wire
     private Label toDate;
 
     @Override
@@ -35,11 +42,10 @@ public class MyComposer extends GenericForwardComposer {
     }
     
     
-    
-    public void onClick$request(Event event) {
+    @Listen("onClick = #request")
+    public void requestButtonOnClick() {
         SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
-        DateFormat dateFormat = DateFormat.getDateInstance();
-        
+
         if (firstDate.getValue() != null) {
             fromDate.setValue(sdf.format(firstDate.getValue()));
         } else {
