1005bc0567511c95938fe80d78eb3198b0be4b68
Artem Pronchakov <artem.pronchakov@calisto.email>
2016-03-01 23:34:48 +0300

    Add Spring WS Skeleton examples

---
+++ Spring/WS/Spring-WS-Client/src/main/java/edu/javacourse/spring/ws/client/Main.java
@@ -0,0 +1,12 @@
+package edu.javacourse.spring.ws.client;
+
+/**
+ * @author Artem Pronchakov <artem.pronchakov@calisto.email>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
