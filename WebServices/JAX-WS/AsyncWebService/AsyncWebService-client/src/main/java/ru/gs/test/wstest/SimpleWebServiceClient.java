d435bdbd410a94beb348390ac611b03d65e00653
Artem Pronchakov <artem.pronchakov@calisto.email>
2016-03-05 00:14:23 +0300

    Add Async JAXWS skeleton

---
+++ WebServices/JAX-WS/AsyncWebService/AsyncWebService-client/src/main/java/ru/gs/test/wstest/SimpleWebServiceClient.java
@@ -0,0 +1,11 @@
+package ru.gs.test.wstest;
+
+import ru.gs.test.SimpleWebService;
+import ru.gs.test.SimpleWebServiceService;
+
+public class SimpleWebServiceClient {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ WebServices/JAX-WS/AsyncWebService/AsyncWebService-ejb/src/main/java/ru/gs/test/AsyncWebService.java
@@ -0,0 +1,17 @@
+package ru.gs.test;
+
+import javax.ejb.AsyncResult;
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.ws.AsyncHandler;
+import java.util.concurrent.Future;
+
+@WebService
+@Stateless
+public class AsyncWebService {
+
+
+
+}
