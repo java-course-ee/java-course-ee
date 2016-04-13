129ca006a85aab37800572b3e965320311491d75
Artem Pronchakov <artem.pronchakov@calisto.email>
2016-04-13 23:20:42 +0300

    Remove unnecessary functionality, move to new api

--- Spring/MVC/GS_Spring_002/src/main/java/com/gemini/spring/entity/User.java
+++ Spring/MVC/GS_Spring_002/src/main/java/com/gemini/spring/entity/User.java
@@ -1,29 +1,20 @@
 package com.gemini.spring.entity;
 
-import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Author: Georgy Gobozov
  * Date: 11.04.13
  */
-@Entity
-@Table(name = "users")
 @XmlRootElement(name = "user")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userId", nullable = false)
     private Long userId;
 
-    @Column(name = "login", nullable = false)
     private String login;
 
-    @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "email", nullable = false)
     private String email;
 
     public User() {
