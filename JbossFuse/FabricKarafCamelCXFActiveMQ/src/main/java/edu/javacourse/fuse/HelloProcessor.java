3dc16bd206419a0d127dfe94b506ff3df6ae4a23
Artem Pronchakov <artem.pronchakov@calisto.email>
2016-09-13 21:02:09 +0300

    Imlement Fuse example

---
+++ JbossFuse/FabricKarafCamelCXFActiveMQ/src/main/java/edu/javacourse/fuse/HelloProcessor.java
@@ -0,0 +1,17 @@
+package edu.javacourse.fuse;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.cxf.message.MessageContentsList;
+
+/**
+ * Created by JavaCourse on 9/13/2016.
+ */
+public class HelloProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        MessageContentsList body = exchange.getIn().getBody(MessageContentsList.class);
+        String o = (String) body.get(0);
+        body.set(0, "Hello " + 0);
+    }
+}
