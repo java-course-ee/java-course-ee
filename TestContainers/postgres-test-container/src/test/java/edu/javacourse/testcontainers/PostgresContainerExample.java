cf6faf64cd3f703ac9e417349fe55836abe697cf
Artem Pronchakov <artem.pronchakov@calisto.email>
2020-02-12 00:06:53 +0300

    Add TestContainers Postgresql example

---
+++ TestContainers/postgres-test-container/src/test/java/edu/javacourse/testcontainers/PostgresContainerExample.java
@@ -0,0 +1,37 @@
+package edu.javacourse.testcontainers;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PostgresContainerExample {
+
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = (PostgreSQLContainer) new PostgreSQLContainer()
+            .withDatabaseName("testdb")
+            .withUsername("sa")
+            .withPassword("sa")
+            .withInitScript("db.sql");
+
+    @Test
+    public void test() {
+        try (
+                Connection connection = DriverManager.getConnection(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(), postgreSQLContainer.getPassword());
+                PreparedStatement statement = connection.prepareStatement("SELECT id, name, birth FROM users");
+                ResultSet resultSet = statement.executeQuery()
+        ) {
+            while (resultSet.next()) {
+                System.out.println("Found user id: " + resultSet.getInt("id") + ", name: " + resultSet.getString("name") + ", birth: " + resultSet.getDate("birth"));
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
